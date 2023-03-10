fa36c2402ada65e866cbb1b5966f06c1b3a27410
小学生 <107250444+xiaopyou@users.noreply.github.com>
2023-03-10 18:13:50 +0800

    测试

---
+++ src/java/sss.java
@@ -0,0 +1,15 @@
+public class sss {
+    public sss(int ss) {
+        this.ss = ss;
+    }
+
+    public int getSs() {
+        return ss;
+    }
+
+    public void setSs(int ss) {
+        this.ss = ss;
+    }
+
+    private int ss;
+}
